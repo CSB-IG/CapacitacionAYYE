d9aa05aba6d948ef7a1097c0eab3a782b42b40ac
gdj_porg <x@desde.porg.com>
2019-02-13 14:51:57 -0600

    filtrado de tabla

--- scripts/ejercicios_dplyr.R
+++ scripts/ejercicios_dplyr.R
@@ -3,3 +3,19 @@ library(data.table)
 
 #cargar datos 
 tabla <- data.table::fread(input = "data/rutas_aereas.txt")
+
+
+#queremos quitar columnas redundantes 
+#usamos dplyr::select
+
+
+tabla2 <- tabla %>% 
+  dplyr::select(Airline, 
+                Source_airport, 
+                Destination_Airport, 
+                Codeshare, 
+                Stops_Number, 
+                Equipment)
+
+
+
