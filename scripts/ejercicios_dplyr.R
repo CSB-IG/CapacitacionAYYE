0c5853a4745844293514a969d2a6faf811b6009d
Yuriko Yamamoto <yamamoto@ciencias.unam.mx>
2019-02-13 14:44:10 -0600

    Quitamos columnas redundantes a tabla

--- scripts/ejercicios_dplyr.R
+++ scripts/ejercicios_dplyr.R
@@ -3,3 +3,16 @@ library(data.table)
 
 #cargar datos 
 tabla <- data.table::fread(input = "data/rutas_aereas.txt")
+tabla
+
+#Queremos quitar columnas redundantes
+#usamos dplyr::select
+tabla2 <- tabla %>%
+    dplyr::select(Airline,
+                  Source_airport,
+                  Destination_Airport,
+                  Codeshare,
+                  Stops_Number,
+                  Equipment
+                  )
+tabla2
